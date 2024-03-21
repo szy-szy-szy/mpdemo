5a995c29201d7a237de3794645f142d4f4061346
szy-szy-szy <2454316000@qq.com>
2024-03-21 22:33:09 +0800

    hotfix commit1

--- src/main/java/com/test/TestMaoPao.java
+++ src/main/java/com/test/TestMaoPao.java
@@ -18,5 +18,6 @@ public class TestMaoPao {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("hot-fix");
+        System.out.println("hot-fix1");
     }
 }
